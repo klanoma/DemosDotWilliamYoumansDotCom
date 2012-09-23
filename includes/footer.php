c094a92a446a9c936fe24d4060932f715a281528
William Youmans <will@williamyoumans.com>
2012-09-22 18:47:57 -0600

    commented out GA

--- includes/footer.php
+++ includes/footer.php
@@ -7,12 +7,13 @@
         <a href="http://www.williamyoumans.com/blog"><i class="icon-undo"></i> back to the blog</a>
       </div>
     </div>
-
+<?php /*
     <script>
       var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
       g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
       s.parentNode.insertBefore(g,s)}(document,'script'));
     </script>
+*/ ?>
   </body>
 </html>
