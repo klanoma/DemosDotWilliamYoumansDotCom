797ed04a9052ffc426e7732dc45830def07be5d7
William Youmans <will@williamyoumans.com>
2012-09-22 18:47:25 -0600

    fixed footer links added robots blocker

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,11 @@
 
     <div id="footer">
       <div class="content">
-        <a href="/contact" class="right">need support? <i class="icon-envelope"></i></a>
-        <a href="/blog"><i class="icon-undo"></i> back to the blog</a>
+        <a href="http://www.williamyoumans.com/contact" class="right">need support? <i class="icon-envelope"></i></a>
+        <a href="http://www.williamyoumans.com/blog"><i class="icon-undo"></i> back to the blog</a>
       </div>
     </div>
 
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
       var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
