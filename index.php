332633343a9b79e7204ab1e145877eb5d8fa966a
William Youmans <will@williamyoumans.com>
2012-09-27 21:40:18 -0600

    canadian map updates

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php $title = 'Programming Demos'; ?>
 
-<?php include("includes/header.php"); ?>
+<?php include('includes/header.php'); ?>
 
 <ul class="menu">
   <li><a href="canada-interactive-map.php">Interactive Javascript Map of Canada</a></li>
 </ul>
 
-<?php include("includes/footer.php"); ?>
+<?php include('includes/footer.php'); ?>
