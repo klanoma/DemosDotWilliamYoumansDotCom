7ecab6cd29a074416d047227e36e35348df8089d
William Youmans <will@williamyoumans.com>
2015-10-31 20:22:43 -0400

    image optim

--- includes/header.php
+++ includes/header.php
@@ -6,17 +6,23 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php echo isset($title) ? $title . ' | ' : ''; ?>Programming Demos | William Youmans</title>
+    <title><?php echo isset($title) ? $title . ' | ' : ''; ?>Programming Demos | William Youmans LLC</title>
     <meta name="description" content="<?php echo isset($title) ? $title : ''; ?> programming demo">
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="/styles/demos.css">
-    <link href="/apple-touch-icon-xlarge.png" rel="apple-touch-icon-precomposed" sizes="144x144" type="image/png"/>
-    <link href="/apple-touch-icon-large.png" rel="apple-touch-icon-precomposed" sizes="114x114" type="image/png"/>
-    <link href="/apple-touch-icon-medium.png" rel="apple-touch-icon-precomposed" sizes="72x72" type="image/png"/>
-    <link href="/apple-touch-icon.png" rel="apple-touch-icon-precomposed" type="image/png"/>
-    <link href="/apple-touch-icon.png" rel="shortcut icon"/>
-    <link href="/favicon.png" rel="shortcut icon" type="image/png"/>
-    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
   </head>
   <body>
     <!--[if lt IE 7]>
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # demos.williamyoumans.com
 
-Here you will find set of demos that are featured on demos.williamyoumans.com and are associated with tutorials from my blog.
+Here you will find set of demos that are featured on demos.williamyoumans.com and
+are associated with tutorials from my blog.
